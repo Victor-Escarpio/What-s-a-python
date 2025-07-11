adc3c6e37b2a1836de18d76bdec9bf070582da8d
Victor Escarpio <213262772+Victor-Escarpio@users.noreply.github.com>
2025-07-10 22:58:59 -0400

    Add files via upload

---
+++ Lists, Tuples, Sets, and Dictionaries.py
@@ -0,0 +1,10 @@
+new = ""
+favorite = ["Steak", "Pizza", "Pasta"]
+while new.lower() != "end": 
+    new = input("Add your new favorite food!: ")
+    favorite.append(new)
+    print(favorite)
+
+print("all done")
+    
+ 
